1587dfd8c84f5dc645b1a23e892eb2a2e22b538a
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-11-23 22:40:59 +0530

    adding upto lec 5

---
+++ 005 - first program/index.js
@@ -0,0 +1,11 @@
+console.log("hello world!");
+
+// to run the source code - node (filename).js
+
+console.log(22+2);
+
+let arr = [1,2,3,4,5];
+
+console.log(arr);
+
+console.log(arr.map((val) => val+5 ));
