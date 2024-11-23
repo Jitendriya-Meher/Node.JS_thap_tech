73a022463bca7f1310ecb720b921c78874af0282
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-11-23 22:56:17 +0530

    adding upto lec 6

---
+++ 006 - Node.js Secrets Why There’s No Window or Document in the Server-Side World!/index.js
@@ -0,0 +1,11 @@
+global.console.log("hii");
+
+globalThis.console.log("hii global this");
+
+globalThis.console.log(module);
+
+console.log(globalThis.process);
+
+console.log(process);
+
+console.log(globalThis.module);
