f4af6a7c3014c2f03e00d26860f1302deedf42ae
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-12-01 16:32:46 +0530

    adding lec 8

---
+++ 008 - Named & Aggregate Exports in Node.js Easily Export and Require Multiple Functions and Variables/index.js
@@ -0,0 +1,11 @@
+const {add, sub} = require('./math1');
+
+const {mul, div, PI} = require("./math2");
+
+console.log('add: ' , add(20,8));
+console.log('sub: ' , sub(20,8));
+
+console.log('mul: ' , mul(20,8));
+console.log('div: ' , div(20,8));
+
+console.log("PI: " , PI)
---
+++ 008 - Named & Aggregate Exports in Node.js Easily Export and Require Multiple Functions and Variables/math2.js
@@ -0,0 +1,11 @@
+const mul = (a,b) => {
+    return a*b;
+}
+
+const div = (a,b) => {
+    return a/b;
+}
+
+const PI = 3.1416;
+
+module.exports = { mul, div, PI};
