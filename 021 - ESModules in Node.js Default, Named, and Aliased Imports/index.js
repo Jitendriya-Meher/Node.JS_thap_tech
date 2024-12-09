0421599a77397776ac0f3b743ef5b6257b7b7af4
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-12-09 10:10:15 +0530

    adding lec 21

---
+++ 021 - ESModules in Node.js Default, Named, and Aliased Imports/index.js
@@ -0,0 +1,11 @@
+import multiply from "./math1.js";
+import {div} from './math1.js';
+import { add} from './math1.js';
+
+import * as math from './math1.js';
+
+console.log('multiply =',multiply(4,9));
+console.log('div =',div(50,4));
+console.log('add =', add(60,56));
+console.log('sub =',math.sub(60,45));
+console.log('Pi =', math.PI);
---
+++ 021 - ESModules in Node.js Default, Named, and Aliased Imports/math1.js
@@ -0,0 +1,21 @@
+const add = (a,b) => {
+    return a + b;
+}
+
+const sub = (a,b) => {
+    return a - b;
+}
+
+const mul = (a,b) => {
+    return a * b;
+}
+
+export const div = (a,b) => {
+    return a / b;
+}
+
+export const PI = 3.1416;
+
+export default mul;
+
+export {add, sub};
